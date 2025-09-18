d7203fdb611993167335860be4653869b94f9c41
Julian Greger <Juliangreger@msn.com>
2025-09-18 15:03:12 +0200

    Update 18-09-2025

---
+++ BracketHubWeb/Pages/Games/GamePage.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BracketHubWeb.Pages.Games
+{
+    public class GamePage : ComponentBase
+    {
+        [Parameter]
+        public string? GameType { get; set; }
+
+        // @page "/game/{GameType}
+    }
+}
---
+++ BracketHubWeb/Services/GameService.cs
@@ -0,0 +1,33 @@
+﻿using BracketHubWeb.Extensions;
+using Microsoft.AspNetCore.Components;
+
+namespace BracketHubWeb.Services
+{
+    public class GameService
+    {
+        public GameService()
+        {
+        }
+
+        public string? GameType { get; private set; }
+        public event EventHandler<string?>? GameChanged;
+
+        public void CheckRoute(RouteData routedata)
+        {
+            if (routedata.RouteValues.TryGetValue(nameof(GameType), out object? value))
+            {
+                string? stringValue = null;
+
+                if (value.IsNotNull() && value is string stringvalue)
+                    stringValue = stringvalue;
+                else
+                    stringValue = null;
+
+                if (stringValue != GameType && GameChanged.IsNotNull())
+                    GameChanged.Invoke(this, stringValue);
+
+                GameType = stringValue;
+            }
+        }
+    }
+}
